594b24c858077b9b84e604026ddab0610b1f91bf
ryouhei <ryouhei05205005@gmail.com>
2019-07-14 03:38:16 +0900

    各クラスを作成

---
+++ Assets/User/Suzuki/Scripts/Question.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Question : MonoBehaviour
+{
+    public Image image;
+    public List<Piece> answer;
+    public List<Piece> OtherPiece;
+    public AnswerColum answerColum;
+    public List<Piece> pieceIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetAnswer(int key)
+    {
+
+    }
+
+    void PieceGenerate(int num)
+    {
+
+    }
+}
