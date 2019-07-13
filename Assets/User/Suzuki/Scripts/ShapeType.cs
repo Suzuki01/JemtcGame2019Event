243ecbb74b012239e16837147db0debd1f4df6fc
ryouhei <ryouhei05205005@gmail.com>
2019-07-14 01:49:41 +0900

    ShapeTypeクラスの追加

---
+++ Assets/User/Suzuki/Scripts/ShapeType.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeType : MonoBehaviour
+{
+
+    // ジャンケンの手の型の列挙型
+    public enum SHAPE_TYPE
+    {
+        Square,    // グー
+        Sphere,  // チョキ
+        Triangle,    // パー
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
