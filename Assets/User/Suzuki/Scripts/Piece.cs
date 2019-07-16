3d224b9d02f4cfeae8aaea60c9ca013574e01d04
ryouhei <ryouhei05205005@gmail.com>
2019-07-16 14:51:04 +0900

    オブジェクトをドラッグで移動できるように

--- Assets/User/Suzuki/Scripts/Piece.cs
+++ Assets/User/Suzuki/Scripts/Piece.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Piece : MonoBehaviour
-{
+[System.Serializable]
+public class Piece : MonoBehaviour {
     public Dictionary<ShapeType, Image> shape;
+ //   public Sprite shape;
     public string String;
     public ShapeType shapeType;
     // Start is called before the first frame update
