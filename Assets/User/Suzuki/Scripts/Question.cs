cfeffe7102e8bc153e7418fdb155c4a19d292597
ryouhei <ryouhei05205005@gmail.com>
2019-08-14 00:18:52 +0900

    ピースの生成時、正解のピースと間違いのピースをシャッフルして一定間隔で表示

--- Assets/User/Suzuki/Scripts/Question.cs
+++ Assets/User/Suzuki/Scripts/Question.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-
+using System.Linq;
+using System;
 
 [System.Serializable]
 public class Question : GameSystem
@@ -12,7 +12,7 @@ public class Question : GameSystem
     public List<Piece> answer;
     public List<Piece> OtherPiece;
     public AnswerColum answerColum;
-    public int pieceIndex;
+    public List<Piece> pieceIndex;
     
     // Start is called before the first frame update
     void Start()
@@ -28,15 +28,20 @@ public class Question : GameSystem
         bool IsAnswer = false;
 
 
-        for(int i = 0; i < pieceIndex; i++)
+        for(int i = 0; i < answer.Count; i++)
         {
             if (answerColum.ColumIndex[i].piece == null)
                 return;
 
             if (answer[i].String != answerColum.ColumIndex[i].piece.String)
+            {
+                Debug.Log(i);
+                Debug.Log(answerColum.ColumIndex[i].piece.String);
+                Debug.Log(answer[i].String);
+                Debug.Log("判定アウト");
                 break;
-
-            else if (i == pieceIndex - 1)
+            }
+            else if (i == pieceIndex.Count - 1)
             {
                 IsAnswer = true;
             } 
@@ -46,7 +51,7 @@ public class Question : GameSystem
         {
             AnswerCountUp();
             IsAnswer = false;
-            
+            Debug.Log("Answer");
         }
     }
 
@@ -59,20 +64,32 @@ public class Question : GameSystem
 
     void PieceGenerate(int num)
     {
-        for(int i = 0; i < OtherPiece.Count; i++)
+        GameObject prefab = (GameObject)Resources.Load("Piece");
+        for (int i = 0; i < OtherPiece.Count; i++)
         {
-            GameObject prefab = (GameObject)Resources.Load("Piece");
-            GameObject obj = Instantiate(prefab, new Vector3(i + i, i + i, 0), Quaternion.identity);
-            Piece piece = obj.GetComponent<Piece>();
-            piece.String = OtherPiece[i].String;
+            pieceIndex.Add(OtherPiece[i]);
         }
 
         for (int i = 0; i < answer.Count; i++)
         {
-            GameObject prefab = (GameObject)Resources.Load("Piece");
-            GameObject obj = Instantiate(prefab, new Vector3(i + i, i + i, 0), Quaternion.identity);
-            Piece piece = obj.GetComponent<Piece>();
-            piece.String = answer[i].String;
+            pieceIndex.Add(answer[i]);            
+        }
+
+        // リストをランダムに並べ替える
+        pieceIndex = pieceIndex.OrderBy(a => Guid.NewGuid()).ToList();
+
+        int posY = 0;
+        int posX = 0;
+        for(int i = 0; i < pieceIndex.Count; i++)
+        {
+            if (i > 5 && i <= 6 || i > 13 && i <= 14)
+            {
+                posY-= 2;
+                posX = -12;
+            }
+
+            GameObject obj = Instantiate(prefab, new Vector3(-4 + i * 2 + posX, -3 + posY , 0), Quaternion.identity);
+            obj.GetComponent<Piece>().String = pieceIndex[i].String;
         }
     }
 }
