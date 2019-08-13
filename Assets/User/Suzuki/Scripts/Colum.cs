92d62e254d5f473bdcc2e0a2cccc260daf609ccb
ryouhei <ryouhei05205005@gmail.com>
2019-08-14 00:19:38 +0900

    カラムから文字が外れたときに、カラムに入ってるピースを外すように

--- Assets/User/Suzuki/Scripts/Colum.cs
+++ Assets/User/Suzuki/Scripts/Colum.cs
@@ -26,5 +26,11 @@ public class Colum : MonoBehaviour
     {
         GameObject obj = GameObject.Find(collider.gameObject.name);
         SetPiece(obj.GetComponent<Piece>());
+        Debug.Log(obj.GetComponent<Piece>().String);
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        piece = null;    
     }
 }
