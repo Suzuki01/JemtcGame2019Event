8fe6a27829b6fa1f0627dd26b9a0c0bc4c0943d2
ryouhei <ryouhei05205005@gmail.com>
2019-08-14 00:16:10 +0900

    カラムの表示位置の調整

--- Assets/User/Suzuki/Scripts/AnswerColum.cs
+++ Assets/User/Suzuki/Scripts/AnswerColum.cs
@@ -20,7 +20,6 @@ public class AnswerColum : Question
     public List<Colum> GetColums()
     {
         Colum[] colums = GetComponents<Colum>();
-        Debug.Log(colums);
 
         return null;
     }
@@ -28,9 +27,37 @@ public class AnswerColum : Question
     public void SetColum(int columNum)
     {
         GameObject prefab = (GameObject)Resources.Load("Colum");
+        float width = 0.0f;
         for (int i = 0; i < columNum; i++)
         {
-            GameObject obj = Instantiate(prefab, new Vector3(i + 1, 0, 0), Quaternion.identity);
+            if (columNum % 2 == 0)
+            {
+                if (i == 0)
+                {
+                    width = -2 * (columNum / 2) + 2 * i;
+                    if (columNum == 4)
+                        width -= 2.0f;
+                }
+                else
+                {
+                    width += 4.0f;
+                }
+            }
+            else
+            {
+                if (i == 0)
+                {  
+                    width = -2 * (columNum / 2) + 2 * i - 1;
+                    if (columNum == 5)
+                        width -= 1;
+                }
+                else
+                {
+                    width += 3.0f;
+                }
+
+            }
+            GameObject obj = Instantiate(prefab, new Vector3(width, 0, 0), Quaternion.identity);
             ColumIndex.Add(obj.GetComponent<Colum>());
         }
     }
